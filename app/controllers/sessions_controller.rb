2a0bb182c4de9f9a65db609a92b398037c799a66
Derrick Mar <derrickmar1215@berkeley.edu>
2014-08-18 17:26:47 -0700

    add session controller destroy and create actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,44 +3,44 @@ class SessionsController < Devise::SessionsController
 	skip_before_filter :verify_authenticity_token, :only => [:create, :destroy]
 
 	# POST /resource/sign_in
-	# def create
-	# 	puts 'IN CREATE SessionsController'
-	# 	self.resource = warden.authenticate!(auth_options)
-	# 	set_flash_message(:notice, :signed_in) if is_flashing_format?
-	# 	sign_in(resource_name, resource)
-	# 	yield resource if block_given?
-	# 	puts current_user.email
-	# 	puts resource
-	# 	puts params
-	# 	respond_to do |format|
-	# 		format.html { respond_with resource, location: after_sign_in_path_for(resource) }
-	# 		format.json { render :json => { user: resource, success: true } }
-	# 	end
-	# end
+	def create
+		puts 'IN CREATE SessionsController'
+		self.resource = warden.authenticate!(auth_options)
+		set_flash_message(:notice, :signed_in) if is_flashing_format?
+		sign_in(resource_name, resource)
+		yield resource if block_given?
+		puts current_user.email
+		puts resource
+		puts params
+		respond_to do |format|
+			format.html { respond_with resource, location: after_sign_in_path_for(resource) }
+			format.json { render :json => { user: resource, success: true } }
+		end
+	end
 
-	# def destroy
-	# 	p 'In DESTROY'
-	# 	puts params
-	# 	puts current_user
-	# 	# binding.pry
-	# 	signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-	# 	puts "is signed_out nil?"
-	# 	puts signed_out
-	# 	set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
-	# 	p 'flash and current_user should print out nil'
-	# 	puts flash[:notice]
-	# 	puts current_user == nil
-	# 	# seems to jump back in front of destroy after this.
-	# 	# yield if block_given?
-	# 	respond_to_on_destroy
-	# end
+	def destroy
+		p 'In DESTROY'
+		puts params
+		puts current_user
+		# binding.pry
+		signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+		puts "is signed_out nil?"
+		puts signed_out
+		set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
+		p 'flash and current_user should print out nil'
+		puts flash[:notice]
+		puts current_user == nil
+		# seems to jump back in front of destroy after this.
+		# yield if block_given?
+		respond_to_on_destroy
+	end
 
-	# def respond_to_on_destroy
-	#     # We actually need to hardcode this as Rails default responder doesn't
-	#     # support returning empty response on GET request
-	#     respond_to do |format|
-	#     	format.json { render :json => { success: true } }
-	#     	format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
-	#     end
-	# end
+	def respond_to_on_destroy
+	    # We actually need to hardcode this as Rails default responder doesn't
+	    # support returning empty response on GET request
+	    respond_to do |format|
+	    	format.json { render :json => { success: true } }
+	    	format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
+	    end
+	end
 end
