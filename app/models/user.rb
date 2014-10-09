e8c02fa9b311742cab915f999046c8706d0b6d5e
Derrick Mar <derrickmar1215@berkeley.edu>
2014-10-09 08:57:42 -0700

    starting happiness project again on october 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
+
+	# allos for token authentication using simple_token_authentication gem
 	acts_as_token_authenticatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable
+
+  	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+	:recoverable, :rememberable, :trackable, :validatable
 end
